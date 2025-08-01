8a7997df8f07b7699d302bea0f331f707bce74c9
WJ-Tien <chc61394@gmail.com>
2025-08-02 00:26:43 +0800

    remove useless stuff

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,4 @@ my_app = FastAPI(title="Cornucopia API", version="1.0.0", root_path="/cornucopia
 my_app.include_router(user_router, prefix="/user", tags=["user"])  
 
 if __name__ == "__main__":
-    # 1. uvicorn app.main:my_app --> uvicorn finds my_app and run 
-    # 2. python -m app.main --> enter the __name__ block and uvicorn.run
     uvicorn.run("app.main:my_app", host="0.0.0.0", port=8000, reload=True)
