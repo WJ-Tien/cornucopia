481f885cdc248f9b55084ca40fc42a2abd16bb9d
WJ-Tien <chc61394@gmail.com>
2025-08-02 18:09:32 +0800

    user login and user refresh token

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 import uvicorn
-from app.routes.user import router as user_router
+from backend.app.routes.user_refresh_token import router as user_refresh_router
+from backend.app.routes.user_login import router as user_login_router
 from fastapi import FastAPI 
 
 my_app = FastAPI(title="Cornucopia API", version="1.0.0", root_path="/cornucopia")
-my_app.include_router(user_router, prefix="/user", tags=["user"])  
+my_app.include_router(user_refresh_router, prefix="/user", tags=["user"])  
+my_app.include_router(user_login_router, prefix="/user", tags=["user"])  
 
 if __name__ == "__main__":
     uvicorn.run("app.main:my_app", host="0.0.0.0", port=8000, reload=True)
---
+++ backend/app/routes/user_refresh_token.py
@@ -0,0 +1,34 @@
+import jwt
+from app.services.user import (
+    create_valid_token, 
+    validate_access_token, 
+)
+from datetime import timedelta
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter()
+
+@router.post("/refresh")
+def refresh_access_token(refresh_token: str):
+    try:
+        payload = validate_access_token(refresh_token)
+        user_id = payload.get("user_id")
+        # short-term access_token
+        new_access_token = create_valid_token(
+            data={"user_id": user_id}, 
+            expires_delta=timedelta(minutes=30)  
+        )
+        # long-term refresh_token
+        new_refresh_token = create_valid_token(
+            data={"user_id": user_id}, 
+            expires_delta=timedelta(days=7)
+        )
+        return {
+            "access_token": new_access_token,
+            "refresh_token": new_refresh_token,  # to replace the old one 
+            "token_type": "bearer"
+        }
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Refresh token expired")
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid refresh token")
